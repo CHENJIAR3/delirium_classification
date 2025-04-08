82ffcc4a5220bff1f6742b6a6c7081b1c6982628
CHENJIAR3 <87299283+CHENJIAR3@users.noreply.github.com>
2025-04-08 15:40:22 +0800

    Add files via upload

--- models/model.py
+++ models/model.py
@@ -13,6 +13,7 @@ from torchvision.models.video import (mc3_18,MC3_18_Weights,
                                       swin3d_b,Swin3D_B_Weights,
                                       swin3d_s,Swin3D_S_Weights,
                                       swin3d_t,Swin3D_T_Weights)
+from thop import profile
 
 class MLPClassifier(nn.Module):
     def __init__(self, input_dim=20, hidden_dim=10, output_dim=2):
@@ -30,64 +31,93 @@ class MLPClassifier(nn.Module):
 
     def forward(self, x):
         x = self.fc1(x)
-        x = self.bn1(x)
         x = self.relu(x)
+        x = self.bn1(x)
         x = self.dropout(x)
         x = self.fc2(x)
-        x = self.bn2(x)
         x = self.relu2(x)
+        x = self.bn2(x)
         x = self.dropout2(x)
         x = self.fc3(x)
         x = self.softmax(x)
         return x
 class VideoNet3D(nn.Module):
-    def __init__(self, num_classes=2,model_type="resnet",fea_dim=20):
+    def __init__(self, args):
         super(VideoNet3D, self).__init__()
-        if model_type == "mc3_18":
+        if args.model_type == "mc3_18":
             self.pretrain_weight = MC3_18_Weights.DEFAULT
-            self.feature_extractor = mc3_18(weights=self.pretrain_weight)
+            if args.pretrained:
+                self.feature_extractor = mc3_18(weights=self.pretrain_weight)
+            else:
+                self.feature_extractor = mc3_18()
+
             # self.feature_extractor.fc = nn.Linear(512, fea_dim)
 
-        if model_type == "mvit_v1":
+        if args.model_type == "mvit_v1":
             self.pretrain_weight = MViT_V1_B_Weights.DEFAULT
-            self.feature_extractor = mvit_v1_b(weights=self.pretrain_weight)
+            if args.pretrained:
+
+                self.feature_extractor = mvit_v1_b(weights=self.pretrain_weight)
+            else:
+                self.feature_extractor = mvit_v1_b()
             # self.feature_extractor.head = nn.Linear(768, fea_dim)
-        if model_type == "mvit_v2":
+        if args.model_type == "mvit_v2":
             self.pretrain_weight = MViT_V2_S_Weights.DEFAULT
-            self.feature_extractor = mvit_v2_s(weights=self.pretrain_weight)
+            if args.pretrained:
+                self.feature_extractor = mvit_v2_s(weights=self.pretrain_weight)
+            else:
+                self.feature_extractor = mvit_v2_s()
             # self.feature_extractor.head = nn.Linear(768, fea_dim)
-        if model_type == "r2+":
+        if args.model_type == "r2+":
             self.pretrain_weight = R2Plus1D_18_Weights.DEFAULT
-            self.feature_extractor = r2plus1d_18(weights=self.pretrain_weight)
+            if args.pretrained:
+                self.feature_extractor = r2plus1d_18(weights=self.pretrain_weight)
+            else:
+                self.feature_extractor = r2plus1d_18()
             # self.feature_extractor.fc = nn.Linear(512, fea_dim)
 
-        if model_type == "resnet":
+        if args.model_type == "resnet":
             self.pretrain_weight = R3D_18_Weights.DEFAULT
-            self.feature_extractor = r3d_18(weights=self.pretrain_weight)
-            # self.feature_extractor.fc = nn.Linear(512, fea_dim)
+            if args.pretrained:
+                self.feature_extractor = r3d_18(weights=self.pretrain_weight)
+            else:
+                self.feature_extractor = r3d_18()
+                # self.feature_extractor.fc = nn.Linear(512, fea_dim)
 
-        if model_type == "s3d":
+        if args.model_type == "s3d":
             self.pretrain_weight = S3D_Weights.DEFAULT
-            self.feature_extractor = s3d(weights=self.pretrain_weight)
+            if args.pretrained:
+                self.feature_extractor = s3d(weights=self.pretrain_weight)
+            else:
+                self.feature_extractor = s3d()
             # self.feature_extractor.classifier = nn.Linear(1024, fea_dim)
 
-        if model_type == "swin_b":
+        if args.model_type == "swin_b":
             self.pretrain_weight = Swin3D_B_Weights.DEFAULT
-            self.feature_extractor = swin3d_b(weights=self.pretrain_weight)
+            if args.pretrained:
+                self.feature_extractor = swin3d_b(weights=self.pretrain_weight)
+            else:
+                self.feature_extractor =  swin3d_b()
             # self.feature_extractor.head = nn.Linear(1024, fea_dim)
-        if model_type == "swin_s":
+        if args.model_type == "swin_s":
             self.pretrain_weight = Swin3D_S_Weights.DEFAULT
-            self.feature_extractor = swin3d_s(weights=self.pretrain_weight)
+            if args.pretrained:
+                self.feature_extractor = swin3d_s(weights=self.pretrain_weight)
+            else:
+                self.feature_extractor =  swin3d_s()
             # self.feature_extractor.head = nn.Linear(1024, fea_dim)
 
-        if model_type == "swin_t":
+        if args.model_type == "swin_t":
             self.pretrain_weight = Swin3D_T_Weights.DEFAULT
-            self.feature_extractor = swin3d_t(weights=self.pretrain_weight)
+            if args.pretrained:
+                self.feature_extractor = swin3d_t(weights=self.pretrain_weight)
+            else:
+                self.feature_extractor =  swin3d_t()
             # self.feature_extractor.head = nn.Linear(1024, fea_dim)
+        # self.freeze_model()
+        self.pool = torch.nn.AdaptiveAvgPool1d(args.fea_dim)
 
-        self.pool = torch.nn.AdaptiveAvgPool1d(fea_dim)
-
-        self.mlp = MLPClassifier(input_dim=fea_dim, hidden_dim=(fea_dim+num_classes)//2,output_dim=num_classes)
+        self.mlp = MLPClassifier(input_dim=args.fea_dim, hidden_dim=(args.fea_dim)//2,output_dim=args.num_classes)
         # self.fc1 = nn.Linear(num_classes, 5*num_classes)
         # self.activation1 = nn.GELU()
         # self.fc2 = nn.Linear(5*num_classes, num_classes)
@@ -115,15 +145,28 @@ class VideoNet3D(nn.Module):
         for i, param in enumerate(self.feature_extractor.parameters()):
             if i != last_layer_index:
                 param.requires_grad = False
-
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() )
 if __name__ =="__main__":
-    x = torch.randn(4,3,16,224,224)
+    x = torch.randn(1,3,16,224,224)
     model_types = ["mc3_18"]
-    # ,"mvit_v1","mvit_v2","r2+",
+    from utils.experimental_setting import args
+
+    # "mc3_18","mvit_v1","mvit_v2","r2+",
     #                    "resnet","s3d","swin_b","swin_s","swin_t"
     for model_type in model_types:
-        model = VideoNet3D(num_classes=2,model_type=model_type)
-        print(model_type,model(x))
+        args.model_type = model_type
+        model = VideoNet3D(args=args)
+    #     print(model_type,model(x))
+    # # model.freeze_model()
+    # print(model(x))
+    print(count_parameters(model))
+    flops, params = profile(model, inputs=(x,),verbose=False)
+    print(f"FLOPs: {flops / 1e9:.2f} G")
+    print(f"Params:{params/1e6:.2f} M")
+    from torchinfo import summary
 
-    # model.freeze_model()
-    print(model(x))
+    summary(model, input_size=x[:1, :, :, :, :].shape)
+
+
+
